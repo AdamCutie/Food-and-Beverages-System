9e17529b79271a1241e2039349131935292653d1
Nishidell <166776404+Nishidell@users.noreply.github.com>
2025-12-13 00:58:53 +0800

    fix the rendering of data in room dining filter

--- frontend/src/pages/Admin/components/AnalyticsDashboard.jsx
+++ frontend/src/pages/Admin/components/AnalyticsDashboard.jsx
@@ -155,10 +155,10 @@ const orderTypeData = (data.orderTypeDistribution || [])
   }));
 
   const topItemsData = data.topSellingItems.map((i) => ({
-    name: i.item_name,
-    sold: i.total_sold,
-    sales: i.total_sales,
-  }));
+  name: i.item_name,
+  sold: Number(i.total_sold), 
+  sales: Number(i.total_sales), 
+}));
 
   return (
     <div className="w-full">
@@ -180,7 +180,7 @@ const orderTypeData = (data.orderTypeDistribution || [])
                 >
                   <option value="All">All Orders</option>
                   <option value="Dine-in">Dine-in</option>
-                  <option value="Room Service">Room Service</option>
+                  <option value="Room Dining">Room Dining</option>
                   <option value="Walk-in">Walk-in</option>
                 </select>
                 <div className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none text-[#3C2A21]">
